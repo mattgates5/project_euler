c7226cdb0b20566e769af5bd0cb887b29d122ff8
Matt Gates <mattgates5@gmail.com>
2014-02-11 14:30:18 -0500

    refactor problem 1

--- problems/problem_1/euler1.rb
+++ problems/problem_1/euler1.rb
@@ -1,7 +1,11 @@
-sum = 0
-(0..999).each do |n|
-  if (n.modulo(3) == 0 || n.modulo(5) == 0)
-    sum += n
-  end
+#sum = 0
+#(0..999).each do |n|
+#  if (n.modulo(3) == 0 || n.modulo(5) == 0)
+#    sum += n
+#  end
+#end
+#puts sum.to_s
+
+def problem1 limit
+  (0...limit).map {|n| (n%3==0 || n%5==0) ? n : 0 }.reduce(:+)
 end
-puts sum.to_s
